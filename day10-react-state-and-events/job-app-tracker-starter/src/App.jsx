93a00eebe133c3f0f02dd0db4a590ac69919507f
Hfear <hannahbfearns@gmail.com>
2023-06-26 14:18:27 -0400

    clickable buttons

--- day10-react-state-and-events/job-app-tracker-starter/src/App.jsx
+++ day10-react-state-and-events/job-app-tracker-starter/src/App.jsx
@@ -1,15 +1,47 @@
 import JobCard from "./JobCard";
 import jobs from "./jobs";
+import classNames from "classnames";
 import "./App.css";
+import { useState } from "react";
+
+const statuses = {
+  1: "Bookmarked",
+  2: "Applying",
+  3: "Applied",
+  4: "Interviewing",
+  5: "Negotiating",
+  6: "Accepted",
+};
+
+
 
 function App() {
+
+  const [selectedStatus, setSelectedStatus] = useState(1);
+
   const jobCards = jobs.map((job, i) => {
     return <JobCard job={job} key={i} />;
   });
 
+  const statusbuttons = Object.keys(statuses).map(statusId => {
+
+    const buttonClass = selectedStatus === parseInt(statusId) ? "bg-blue-500": ""
+
+    return (
+      <button key={statusId} 
+      className={buttonClass}
+      onClick = {() => setSelectedStatus(parseInt(statusId))}
+      > 
+      
+      {statuses[statusId]}
+      </button>
+    )
+  })
+
   return (
     <div className="mx-auto max-w-4xl">
       <h1>Job Application Tracker</h1>
+      <div className="">{statusbuttons}</div>
       {jobCards}
     </div>
   );
---
+++ day10-react-state-and-events/job-app-tracker-starter/src/App.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from './App';
+
+test('renders button group correctly', () => {
+  render(<App />);
+
+  expect(screen.getByRole('button', { name: 'Bookmarked' })).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: 'Applying' })).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: 'Applied' })).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: 'Interviewing' })).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: 'Negotiating' })).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: 'Accepted' })).toBeInTheDocument();
+});
+
+test("clicking a button makes its filter active ")
